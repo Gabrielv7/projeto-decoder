19bdadc5bcc4487724a9bc39a1b01cf4cdf7e470
Gabriel Costa Afonso <gabriel.afonso@pampa.compasso>
2022-04-04 14:39:35 -0300

    Principais Bibliotecas de Logging, Log Levels e Logging no Spring com Logback

--- authuser/src/main/java/com/ead/authuser/api/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/api/controllers/AuthenticationController.java
@@ -4,10 +4,13 @@ import com.ead.authuser.api.mapper.UserMapper;
 import com.ead.authuser.domain.dtos.UserDto;
 import com.ead.authuser.domain.forms.UserForm;
 import com.ead.authuser.domain.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     @Autowired
     UserService userService;
@@ -34,6 +39,19 @@ public class AuthenticationController {
 
         return userMapper.toDto(userService.save(userModel));
 
+    }
+
+
+    @GetMapping("/")
+    public String index(){
+
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARN");
+        logger.error("ERROR");
+
+        return "Logging Spring Boot...";
     }
 
 }
