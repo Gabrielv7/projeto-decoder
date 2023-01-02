31bdad90d0f283b3fc72e9ef128c2b06326ab723
Gabriel Costa <gabrielcosta.v7@gmail.com>
2023-01-02 13:34:37 -0300

    Hipermídias com Spring Hateoas

--- authuser/src/main/java/com/ead/authuser/domain/dto/response/UserResponse.java
+++ authuser/src/main/java/com/ead/authuser/domain/dto/response/UserResponse.java
@@ -6,13 +6,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class UserResponse {
+public class UserResponse extends RepresentationModel<UserResponse> {
 
     private UUID userId;
 
