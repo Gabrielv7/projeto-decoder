1bb5be02d1bc25bae6f4cbcf362e0ae7803e2841
Gabriel Costa <gabrielcosta.v7@gmail.com>
2023-01-02 17:38:49 -0300

    Mapear Entidades, criar Repositories e Services para Multiplas Entidades

---
+++ course/src/main/java/com/ead/course/domain/Lesson.java
@@ -0,0 +1,58 @@
+package com.ead.course.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_LESSON")
+public class Lesson implements Serializable {
+
+    //Remover as anotações do JSON quando for criado a classe DTO
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID lessonId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 250)
+    private String description;
+
+    @Column(nullable = false)
+    private String videoUrl;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDate;
+
+}
