332f820c9fc4aa5fa0261c060704d0647213d477
Gabriel Costa <gabrielcosta.v7@gmail.com>
2023-01-16 15:48:48 -0300

    Boas Práticas de Datas nas APIs utilizando o Padrão ISO 8601 UTC

--- course/src/main/java/com/ead/course/domain/dto/response/LessonResponse.java
+++ course/src/main/java/com/ead/course/domain/dto/response/LessonResponse.java
@@ -19,10 +19,10 @@ public class LessonResponse {
 
     private String videoUrl;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime creationDate;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime lastUpdateDate;
 
 }
