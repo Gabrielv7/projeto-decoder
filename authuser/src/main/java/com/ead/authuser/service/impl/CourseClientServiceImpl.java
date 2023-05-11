fea8434aaae03797ec4f59de9d58b14e6fa320e1
Gabriel Costa <gabrielcosta.v7@gmail.com>
2023-05-11 14:28:29 -0300

    Implementar Circuit Breaker Resilience4j

--- authuser/src/main/java/com/ead/authuser/service/impl/CourseClientServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/service/impl/CourseClientServiceImpl.java
@@ -5,15 +5,13 @@ import com.ead.authuser.domain.dto.response.CourseResponse;
 import com.ead.authuser.service.CourseClientService;
 import com.ead.authuser.service.UserService;
 import com.ead.authuser.util.ConstantsLog;
-import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 @Log4j2
@@ -26,22 +24,16 @@ public class CourseClientServiceImpl implements CourseClientService {
     @Autowired
     private UserService userService;
 
+    @CircuitBreaker(name = "circuitbreakerInstance")
     @Override
     public Page<CourseResponse> getAllCoursesByUserId(UUID userId, Pageable pageable) {
-        Page<CourseResponse> result = null;
-        try {
 
-            userService.findById(userId);
+        userService.findById(userId);
 
-            log.info(ConstantsLog.LOG_METHOD + ConstantsLog.LOG_EVENT + ConstantsLog.LOG_USER_ID,
-                    "getAllCoursesByUserId", "request ms-course", userId);
+        log.info(ConstantsLog.LOG_METHOD + ConstantsLog.LOG_EVENT + ConstantsLog.LOG_USER_ID,
+                "getAllCoursesByUserId", "request ms-course", userId);
 
-            result = courseClient.getAllCourses(userId, pageable);
-            
-        }catch (FeignException ex){
-            log.error("FeignException = {}", ex.getMessage());
-        }
-        return result == null ? new PageImpl<>(new ArrayList<>()) : result;
+        return courseClient.getAllCourses(userId, pageable);
     }
 
 }
