5110c731dd984acaf0731d8c0c7cc080446af9f4
Gabriel Costa <gabrielcosta.v7@gmail.com>
2023-06-06 20:14:20 -0300

    Preparar comunicação assíncrona  via comando  no ms notification

---
+++ notification/src/main/java/com/ead/notification/domain/Notification.java
@@ -0,0 +1,56 @@
+package com.ead.notification.domain;
+
+import com.ead.notification.domain.enums.StatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+@Entity
+@Table(name = "TB_NOTIFICATION")
+public class Notification implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID notificationId;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false)
+    private String message;
+
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusEnum statusEnum;
+
+}
---
+++ notification/src/main/java/com/ead/notification/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.ead.notification.repository;
+
+import com.ead.notification.domain.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+}
