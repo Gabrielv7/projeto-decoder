45dfcc9d12d5b87b8ab234e6bfbe0ac1714ea3be
Gabriel Costa <gabrielcosta.v7@gmail.com>
2023-08-07 14:07:21 -0300

    Refatorando nome de classes, e ajustando as classes de erro

--- notification/src/main/java/com/ead/notification/dto/response/ErrorResponse.java
+++
@@ -1,9 +0,0 @@
-package com.ead.notification.dto.response;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(value = JsonInclude.Include.NON_NULL)
-public record ErrorResponse(int code,
-                            String message) {
-
-}
--- notification/src/main/java/com/ead/notification/exception/FieldError.java
+++
@@ -1,6 +0,0 @@
-package com.ead.notification.exception;
-
-public record FieldError(String message,
-                         String field) {
-
-}
